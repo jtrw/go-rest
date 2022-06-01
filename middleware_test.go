e060af5652e96a87f9109b7ad4ec6cad67409271
Nil Borodulia <nil.b@oxtech.org>
2022-06-01 19:32:28 +0100

    add POST middleware

--- middleware_test.go
+++ middleware_test.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"bytes"
+	"encoding/json"
 )
 
 func TestMiddleware_Ping(t *testing.T) {
@@ -33,4 +35,27 @@ func TestMiddleware_Ping(t *testing.T) {
 	b, err = io.ReadAll(resp.Body)
 	assert.NoError(t, err)
 	assert.Equal(t, "blabla blabla", string(b))
+}
+
+func TestMiddleware_PingPost(t *testing.T) {
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("blabla blabla"))
+		require.NoError(t, err)
+	})
+	ts := httptest.NewServer(Ping(handler))
+	defer ts.Close()
+
+    var jsonData = []byte("")
+
+	resp, err := http.Post(ts.URL + "/ping", "application/json", bytes.NewBuffer(jsonData))
+	require.Nil(t, err)
+	assert.Equal(t, 200, resp.StatusCode)
+	defer resp.Body.Close()
+
+	var res map[string]interface{}
+
+    json.NewDecoder(resp.Body).Decode(&res)
+
+	assert.NoError(t, err)
+	assert.Equal(t, "PONG", res["message"])
 }
