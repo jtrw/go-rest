237333959397a330f19decbf16107f12e0af9025
Nil Borodulia <nil.borodulia@gmail.com>
2024-01-22 11:14:15 +0300

    Add Ping, PanicRecovery, and AppInfo middlewares

--- middleware.go
+++ middleware.go
@@ -1,11 +1,11 @@
 package rest
 
 import (
-    "log"
-	"net/http"
-	"strings"
 	"encoding/json"
+	"log"
+	"net/http"
 	"runtime/debug"
+	"strings"
 )
 
 // Ping middleware response with pong to /ping. Stops chain if ping request detected
@@ -18,28 +18,42 @@ func Ping(next http.Handler) http.Handler {
 			return
 		}
 		if r.Method == "POST" && strings.HasSuffix(strings.ToLower(r.URL.Path), "/ping") {
-            w.Header().Set("Content-Type", "application/json")
-            w.WriteHeader(http.StatusOK)
-            resp := make(map[string]string)
-            resp["message"] = "PONG"
-            jsonResp, _ := json.Marshal(resp)
-            _, _ = w.Write(jsonResp)
-            return
-        }
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			resp := make(map[string]string)
+			resp["message"] = "PONG"
+			jsonResp, _ := json.Marshal(resp)
+			_, _ = w.Write(jsonResp)
+			return
+		}
 		next.ServeHTTP(w, r)
 	}
 	return http.HandlerFunc(fn)
 }
 
 func PanicRecovery(next http.Handler) http.Handler {
-  return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-    defer func() {
-      if err := recover(); err != nil {
-        http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-        log.Println("An error occurred:", err)
-        log.Println(string(debug.Stack()))
-      }
-    }()
-    next.ServeHTTP(w, req)
-  })
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+				log.Println("An error occurred:", err)
+				log.Println(string(debug.Stack()))
+			}
+		}()
+		next.ServeHTTP(w, req)
+	})
+}
+
+func AppInfo(app, author, version string) func(http.Handler) http.Handler {
+	f := func(h http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Author", author)
+			w.Header().Set("App-Name", app)
+			w.Header().Set("App-Version", version)
+
+			h.ServeHTTP(w, r)
+		}
+		return http.HandlerFunc(fn)
+	}
+	return f
 }
