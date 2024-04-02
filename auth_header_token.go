85c104507ff6c838f03d70aeb6fb62a93713ecb2
Nil Borodulia <nil.borodulia@gmail.com>
2024-04-02 12:46:15 +0200

    Refactor JWT authentication and add tests

--- auth_header_token.go
+++ auth_header_token.go
@@ -1,18 +1,19 @@
 package rest
 
 import (
-    "net/http"
+	"net/http"
 )
 
 func Authentication(headerName, token string) func(http.Handler) http.Handler {
-    return func(next http.Handler) http.Handler {
-        fn := func(w http.ResponseWriter, r *http.Request) {
-            apiToken := r.Header.Get(headerName)
-            if apiToken != token {
-                http.Error(w, "Unauthorized", http.StatusUnauthorized)
-                return
-            }
-        }
-        return http.HandlerFunc(fn)
-    }
+	return func(next http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			apiToken := r.Header.Get(headerName)
+			if apiToken != token {
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				return
+			}
+			next.ServeHTTP(w, r)
+		}
+		return http.HandlerFunc(fn)
+	}
 }
--- auth_header_token_test.go
+++ auth_header_token_test.go
@@ -1,42 +1,42 @@
 package rest
 
 import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"io"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestHeaderTokenAuth(t *testing.T) {
-    handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        _, err := w.Write([]byte("blabla blabla"))
-        require.NoError(t, err)
-    })
-    headerName := "Api-Token"
-    ts := httptest.NewServer(Authentication("Api-Token", "1234567890")(handler))
-    defer ts.Close()
-    {
-        req, err := http.NewRequest("GET", ts.URL+"/ping", nil)
-        require.NoError(t, err)
-        req.Header.Set(headerName, "1234567890")
-        resp, err := http.DefaultClient.Do(req)
-        require.NoError(t, err)
-        assert.Equal(t, 200, resp.StatusCode)
-        defer resp.Body.Close()
-    }
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("blabla blabla"))
+		require.NoError(t, err)
+	})
+	headerName := "Api-Token"
+	ts := httptest.NewServer(Authentication("Api-Token", "1234567890")(handler))
+	defer ts.Close()
+	{
+		req, err := http.NewRequest("GET", ts.URL+"/ping", nil)
+		require.NoError(t, err)
+		req.Header.Set(headerName, "1234567890")
+		resp, err := http.DefaultClient.Do(req)
+		require.NoError(t, err)
+		assert.Equal(t, 200, resp.StatusCode)
+		defer resp.Body.Close()
+	}
 
-     {
-        req, err := http.NewRequest("GET", ts.URL+"/ping", nil)
-        require.NoError(t, err)
-        req.Header.Set(headerName, "invalid")
-        resp, err := http.DefaultClient.Do(req)
-        require.NoError(t, err)
-        assert.Equal(t, http.StatusUnauthorized, resp.StatusCode)
-        defer resp.Body.Close()
-        b, err := io.ReadAll(resp.Body)
-        assert.NoError(t, err)
-        assert.Equal(t, "Unauthorized\n", string(b))
-     }
+	{
+		req, err := http.NewRequest("GET", ts.URL+"/ping", nil)
+		require.NoError(t, err)
+		req.Header.Set(headerName, "invalid")
+		resp, err := http.DefaultClient.Do(req)
+		require.NoError(t, err)
+		assert.Equal(t, http.StatusUnauthorized, resp.StatusCode)
+		defer resp.Body.Close()
+		b, err := io.ReadAll(resp.Body)
+		assert.NoError(t, err)
+		assert.Equal(t, "Unauthorized\n", string(b))
+	}
 }
